6db9a6e52b67ae4b4b26e6a0cdb1b2bcd40c6db1
CT2JAY <pjao.ct2jay@gmail.com>
2019-03-11 21:13:46 +0000

    trata

---
+++ TRATA.sh
@@ -0,0 +1,1 @@
+ sudo systemctl stop xlxd.service ambed.service ; sudo rm /xlxd/dmrid.dat; sudo cp ambed/ambed /usr/local/bin/ambed ; sudo cp src/xlxd /xlxd/xlxd ; sudo systemctl start xlxd.service ambed.service
